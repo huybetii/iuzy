be0754bb2a8356c581ffd4dd4cdaa35fd1184bfa
huybetii <88920842+huybetii@users.noreply.github.com>
2021-08-14 18:35:10 +0700

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const CONFIG = {
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích Hy <33',
     btnNo: 'Không, iêm  trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao Zy lại yêu hy <3',
     btnReply: 'Gửi cho hy <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Hy biết mà. Yêu iêm  nhiều nhiều',
